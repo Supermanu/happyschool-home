08368982e44c11ce75089e6a776c5e00574d3570
Supermanu <manueltondeur@gmail.com>
2021-09-14 09:52:58 +0200

    Enable searching by subject

--- assets/home/emails.vue
+++ assets/home/emails.vue
@@ -7,7 +7,32 @@
             <b-list-group flush>
                 <b-list-group-item>
                     <strong>Derniers messages</strong>
+                    <b-btn
+                        size="sm"
+                        variant="outline-primary"
+                        v-b-toggle.searchemails
+                    >
+                        <b-icon
+                            icon="search"
+                        />
+                    </b-btn>
                 </b-list-group-item>
+                <b-collapse id="searchemails">
+                    <b-list-group-item>
+                        <b-input-group>
+                            <template #prepend>
+                                <b-input-group-text>
+                                    <b-icon icon="search" />
+                                </b-input-group-text>
+                            </template>
+                            <b-form-input
+                                v-model="search"
+                                @input="getEmails"
+                                placeholder="Rechercher un courriel"
+                            />
+                        </b-input-group>
+                    </b-list-group-item>
+                </b-collapse>
                 <b-skeleton-wrapper :loading="loading">
                     <template #loading>
                         <b-list-group-item>
@@ -57,19 +82,25 @@ export default {
             loading: true,
             emails: [],
             currentPage: 1,
-            emailCount: 0
+            emailCount: 0,
+            search: "",
+            searchId: -1,
         };
     },
     watch: {
         currentPage: function () {
             this.getEmails();
-        }
+        },
     },
     methods: {
         getEmails: function () {
             this.loading = true;
-            axios.get(`/mail_notification/api/notif/?page_size=5&page=${this.currentPage}`)
+            this.searchId += 1;
+            const currentSearch = this.searchId;
+            axios.get(`/mail_notification/api/notif/?page_size=5&page=${this.currentPage}&search=${this.search}`)
                 .then(resp => {
+                    if (this.searchId !== currentSearch)
+                        return;
                     this.emails = resp.data.results;
                     this.emailCount = resp.data.count;
                     this.loading = false;
@@ -77,7 +108,7 @@ export default {
                 .catch(err => {
                     this.loading = false;
                     console.log(err);
-                })
+                });
         }
     },
     mounted: function () {
